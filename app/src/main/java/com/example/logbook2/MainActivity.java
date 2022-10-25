db883f5bbd613301b094cd7a0c063d532c75d3d1
unknown <nguyenhungthinh2504@gmail.com>
2022-10-25 16:20:04 +0700

    ex2

--- app/src/main/java/com/example/logbook2/MainActivity.java
+++ app/src/main/java/com/example/logbook2/MainActivity.java
@@ -2,22 +2,40 @@ package com.example.logbook2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.logbook2.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    Handler mainHandler = new Handler();
+    ProgressDialog progressDialog;
 
     ImageView imageView;
     Button backward, forward;
     int i = 0;
-    private int[] textureArrayWin = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five};
+
+
+    private int[] changeImage = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
 
         imageView = (ImageView) findViewById(R.id.imageView);
         backward = (Button) findViewById(R.id.backward);
@@ -36,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(i > 0) {
                     i--;
-                    imageView.setImageResource(textureArrayWin[i]);
+                    imageView.setImageResource(changeImage[i]);
                 }
             }
         });
@@ -46,8 +64,57 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (i < 4) {
                     i++;
-                    imageView.setImageResource(textureArrayWin[i]);
+                    imageView.setImageResource(changeImage[i]);
                 }
+            }
+        });
+
+        class AddImage extends Thread{
+
+            String URL;
+            Bitmap bitmap;
+
+            AddImage(String URL){
+                this.URL = URL;
+            }
+
+            @Override
+            public void run() {
+                mainHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog = new ProgressDialog(MainActivity.this);
+                        progressDialog.setMessage("Getting your pic...");
+                        progressDialog.setCancelable(false);
+                        progressDialog.show();
+                    }
+                });
+
+                InputStream inputStream = null;
+                try {
+                    inputStream = new java.net.URL(URL).openStream();
+                    bitmap = BitmapFactory.decodeStream(inputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mainHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                        binding.imageView.setImageBitmap(bitmap);
+                    }
+                });
+
+            }
+        }
+
+        binding.addlinkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = binding.etURL.getText().toString();
+                new AddImage(url).start();
             }
         });
     }
